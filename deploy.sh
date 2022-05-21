b86d42f8a7ea80e7007337f694cd01413cc01bee
Meng Xin <mengxin891029@gmail.com>
2022-05-22 02:43:46 +0800

    updated sb server

--- deploy.sh
+++ deploy.sh
@@ -2,61 +2,13 @@
 #                                                                 #
 #                    Start of Kubenetes Update                    #
 #                                                                 #
-#                         Namespace: web                          #
+#                         Namespace: default                      #
 #                                                                 #
 ###################################################################
 
 
-# update sea-mengxin-pink
-docker buildx build --platform linux/amd64,linux/arm64 --push -t mengxin891029/sea-mengxin-pink:latest -t mengxin891029/sea-mengxin-pink:$GIT_SHA -f ./docker-images/sea/Dockerfile ./docker-images/sea
-# docker push mengxin891029/sea-mengxin-pink:$GIT_SHA
-# docker push mengxin891029/sea-mengxin-pink:latest
-kubectl apply -f k8s/web/sea-mengxin-pink
-kubectl set image --namespace web deployments/sea-mengxin-pink-deployment sea-mengxin-pink=mengxin891029/sea-mengxin-pink:$GIT_SHA
-
-
-# update chat-mengxin-pink
-docker buildx build --platform linux/amd64,linux/arm64 --push -t mengxin891029/chat-mengxin-pink:latest -t mengxin891029/chat-mengxin-pink:$GIT_SHA -f ./docker-images/chat/Dockerfile ./docker-images/chat
-# docker push mengxin891029/chat-mengxin-pink:$GIT_SHA
-# docker push mengxin891029/chat-mengxin-pink:latest
-kubectl apply -f k8s/web/chat-mengxin-pink
-kubectl set image --namespace web deployments/chat-mengxin-pink-deployment chat-mengxin-pink=mengxin891029/chat-mengxin-pink:$GIT_SHA
-
-
-# # update grpc-mengxin-ml
-# # docker build -t mengxin891029/grpc-mengxin-ml:latest -t mengxin891029/grpc-mengxin-ml:$GIT_SHA -f ./docker-images/grpc/Dockerfile ./docker-images/grpc
-# # docker push mengxin891029/grpc-mengxin-ml:$GIT_SHA
-# # docker push mengxin891029/grpc-mengxin-ml:latest
-# docker buildx build --platform linux/amd64,linux/arm64 --push -t mengxin891029/grpc-mengxin-ml:latest -t mengxin891029/grpc-mengxin-ml:$GIT_SHA -f ./docker-images/grpc/Dockerfile ./docker-images/grpc
-# kubectl apply -f k8s/web/grpc-mengxin-ml
-# kubectl set image --namespace web deployments/grpc-mengxin-ml-deployment grpc-mengxin-ml=mengxin891029/grpc-mengxin-ml:$GIT_SHA
-
-
-# update httpbin-mengxin-pink
-kubectl apply -f k8s/web/httpbin-mengxin-pink
-
-
-# # update mongo-express-mengxin-ml
-# kubectl apply -f k8s/web/mongo-express-mengxin-ml
-
-# # update jupyter-mengxin-ml
-# # docker build -t mengxin891029/jupyter-mengxin-ml:latest -t mengxin891029/jupyter-mengxin-ml:$GIT_SHA -f ./docker-images/jupyter/Dockerfile ./docker-images/jupyter
-# # docker push mengxin891029/jupyter-mengxin-ml:$GIT_SHA
-# # docker push mengxin891029/jupyter-mengxin-ml:latest
-# docker buildx build --platform linux/amd64,linux/arm64 --push -t mengxin891029/jupyter-mengxin-ml:latest -t mengxin891029/jupyter-mengxin-ml:$GIT_SHA -f ./docker-images/jupyter/Dockerfile ./docker-images/jupyter
-# kubectl apply -f k8s/web/jupyter-mengxin-ml
-# kubectl set image --namespace web deployments/jupyter-mengxin-ml-deployment jupyter-mengxin-ml=mengxin891029/jupyter-mengxin-ml:$GIT_SHA
-
-# update tunnel-mengxin-pink
-kubectl apply -f k8s/web/tunnel-mengxin-pink
-
-
-# # update general kubernetes namespace: web
-# kubectl apply -f k8s/web
-
-
-
-
+# update mengxin-local-default-ingress
+kubectl apply -f k8s/web/mengxin-local-default-ingress.yaml
 
 
 ###################################################################
@@ -66,19 +18,6 @@ kubectl apply -f k8s/web/tunnel-mengxin-pink
 #                         Namespace: tcp                          #
 #                                                                 #
 ###################################################################
-
-
-# # update ssl-mengxin-ml-keyless
-# # docker build -t mengxin891029/keyless:latest -t mengxin891029/keyless:$GIT_SHA -f ./docker-images/keyless/Dockerfile ./docker-images/keyless
-# # docker push mengxin891029/keyless:$GIT_SHA
-# # docker push mengxin891029/keyless:latest
-# docker buildx build --platform linux/amd64,linux/arm64 --push -t mengxin891029/keyless:latest -t mengxin891029/keyless:$GIT_SHA -f ./docker-images/keyless/Dockerfile ./docker-images/keyless
-# kubectl apply -f k8s/tcp/ssl-mengxin-ml-keyless
-# kubectl set image --namespace tcp deployments/ssl-mengxin-ml-keyless-deployment ssl-mengxin-ml=mengxin891029/keyless:$GIT_SHA
-
-
-# # update common-storage-nfs
-# kubectl apply -f k8s/tcp/common-storage-nfs
 
 
 # # update mengxin-sea1-test-vm
@@ -98,3 +37,64 @@ kubectl apply -f k8s/tcp/redis-mengxin-pink
 # # update general kubernetes namespace: tcp
 # kubectl delete validatingwebhookconfiguration admission.voyager.appscode.com
 # kubectl apply -f k8s/tcp
+
+
+
+
+###################################################################
+#                                                                 #
+#                    Start of Kubenetes Update                    #
+#                                                                 #
+#                         Namespace: web                          #
+#                                                                 #
+###################################################################
+
+
+# update chat-mengxin-pink
+docker buildx build --platform linux/amd64,linux/arm64 --push -t mengxin891029/chat-mengxin-pink:latest -t mengxin891029/chat-mengxin-pink:$GIT_SHA -f ./docker-images/chat/Dockerfile ./docker-images/chat
+# docker push mengxin891029/chat-mengxin-pink:$GIT_SHA
+# docker push mengxin891029/chat-mengxin-pink:latest
+kubectl apply -f k8s/web/chat-mengxin-pink
+kubectl set image --namespace web deployments/chat-mengxin-pink-deployment chat-mengxin-pink=mengxin891029/chat-mengxin-pink:$GIT_SHA
+
+
+# update dns-mengxin-pink
+kubectl apply -f k8s/web/dns-mengxin-pink
+
+
+# # update grpc-mengxin-ml
+# # docker build -t mengxin891029/grpc-mengxin-ml:latest -t mengxin891029/grpc-mengxin-ml:$GIT_SHA -f ./docker-images/grpc/Dockerfile ./docker-images/grpc
+# # docker push mengxin891029/grpc-mengxin-ml:$GIT_SHA
+# # docker push mengxin891029/grpc-mengxin-ml:latest
+# docker buildx build --platform linux/amd64,linux/arm64 --push -t mengxin891029/grpc-mengxin-ml:latest -t mengxin891029/grpc-mengxin-ml:$GIT_SHA -f ./docker-images/grpc/Dockerfile ./docker-images/grpc
+# kubectl apply -f k8s/web/grpc-mengxin-ml
+# kubectl set image --namespace web deployments/grpc-mengxin-ml-deployment grpc-mengxin-ml=mengxin891029/grpc-mengxin-ml:$GIT_SHA
+
+
+# update httpbin-mengxin-pink
+kubectl apply -f k8s/web/httpbin-mengxin-pink
+
+
+# update sea-mengxin-pink
+docker buildx build --platform linux/amd64,linux/arm64 --push -t mengxin891029/sea-mengxin-pink:latest -t mengxin891029/sea-mengxin-pink:$GIT_SHA -f ./docker-images/sea/Dockerfile ./docker-images/sea
+# docker push mengxin891029/sea-mengxin-pink:$GIT_SHA
+# docker push mengxin891029/sea-mengxin-pink:latest
+kubectl apply -f k8s/web/sea-mengxin-pink
+kubectl set image --namespace web deployments/sea-mengxin-pink-deployment sea-mengxin-pink=mengxin891029/sea-mengxin-pink:$GIT_SHA
+
+
+# update tunnel-mengxin-pink
+kubectl apply -f k8s/web/tunnel-mengxin-pink
+
+
+# update mengxin-local-web-ingress
+kubectl apply -f k8s/web/mengxin-local-web-ingress.yaml
+
+# # update general kubernetes namespace: web
+# kubectl apply -f k8s/web
+
+
+
+
+
+
